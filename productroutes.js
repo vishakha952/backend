d5d4c080082fcfb75cc91cbec60fe0681bd94536
Vishakha Thakur <vishakhat895@gmail.com>
2024-08-03 21:22:52 +0530

    Add files via upload

---
+++ productroutes.js
@@ -0,0 +1,9 @@
+const productcontroller=require("../controllers/productcontroller")
+const express=require("express");
+const router=express.Router();
+router.get('/',productcontroller )
+router.get('/name/:name',productcontroller )
+router.get('/category/:category',productcontroller )
+
+
+module.exports=router
